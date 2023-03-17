077d248ff550f004fcc92f51d3c2ef7a0f993cc3
zidangoodmood <ziidan15januri@gmail.com>
2023-03-17 10:09:02 +0700

    project web prog

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo"<h1>Perkenalkan</h1>";
+        echo"Nama saya Zidan Januri Zumantara
+        saya tinggal di daerah cengkareng
+        olahraga yang saya sukai adalah 
+        Bulu Tangkis";
+    }
+}
